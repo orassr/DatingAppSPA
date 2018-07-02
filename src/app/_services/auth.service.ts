667e3e28c23e41535b47d2e227df184c80231635
orassr <orassraf@gmail.com>
2018-07-02 14:12:57 -0700

    work

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,11 @@
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { observable, Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -19,16 +24,35 @@ constructor(private http: Http) { }
         localStorage.setItem('token', user.tokenString);
         this.userToken = user.tokenString;
       }
-    });
+    }).catch(this.handleError);
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+    return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
   }
 
   private requestOptions() {
     const headers = new Headers({ 'Content-type': 'application/json' });
     return new RequestOptions({headers: headers});
+  }
+
+  private handleError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+    if (applicationError) {
+      return Observable.throw(applicationError);
+    }
+    const serverError = error.json();
+    let modelStateError = '';
+    if (serverError) {
+      for (const key in serverError) {
+        if (serverError[key]) {
+          modelStateError += serverError[key] + '\n';
+        }
+      }
+    }
+    return Observable.throw(
+      modelStateError || 'Server error'
+    );
   }
 
 
