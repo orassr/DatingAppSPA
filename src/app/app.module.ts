a76b4bf06cbbace28875f62d00312e0413bb2ace
orassr <orassraf@gmail.com>
2018-07-15 11:48:55 -0700

    Adding TimeAgoPipe + ActionFilter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 @NgModule({
    declarations: [
@@ -43,7 +44,8 @@ import { FileUploadModule } from 'ng2-file-upload';
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      TimeAgoPipe
    ],
    imports: [
       BrowserModule,
